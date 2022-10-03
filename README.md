ac41b646e9f3f4720e4571a4cca5889ca6e0bf02
sauravhathi <61316762+sauravhathi@users.noreply.github.com>
2022-10-03 17:37:53 +0530

    how to use

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Hitbullseye-Automate-Tool
- Hitbullseye Automate Tool
+# Hitbullseye Automate Tool
+
+## How to use
+
+Change the keys value to your test keys and run the script in the browser console.
+
+### Note: Install Absolute Enable Right Click extension in your browser to use the script.
+
+  1. Open the test in your browser
+  2. Open the console in your browser
+  3. Copy the script and paste it in the console
+  4. Press enter
+  5. Wait for the test to be submitted
--- script.js
+++ script.js
@@ -22,3 +22,15 @@ const inter = setInterval(
   // 2 seconds(2000 milliseconds) for each question
 ,2000);
 
+
+## How to use
+
+Change the keys value to your test keys and run the script in the browser console.
+
+Note: Install Absolute Enable Right Click extension in your browser to use the script.
+
+  1. Open the test in your browser
+  2. Open the console in your browser
+  3. Copy the script and paste it in the console
+  4. Press enter
+  5. Wait for the test to be submitted
