67672704d1cbe3265f94fe33ee691420f0bc94d2
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2023-01-11 19:34:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-
-// chanage keys value
-const ans = "CABDAAAAACDAADBABAABCAABDBABBCDABCCBCDAD";
+const ans = "AADACADCBDCBAACDABDCACBACACBAA";
 const len = ans.length;
 
 let i = 0;
 
 const inter = setInterval(
   () => {
-    document.querySelector(`#${ans[i]}_${i+1}`).click(); // select the correct option
+     document.querySelector(`#${ans[i]}_${i+1}`).click();
     document.querySelector("#main_div > div.tableWidthPercent > div.onlineTestLeftDiv > div.qnav > span.saveNextButton > a").click(); // move to next question
     i++;
     
     if(i == len){ // stop the interval when all questions are answered
       clearInterval(inter)
-      document.querySelector("#activator").click(); // submit the test
-      document.querySelector(`#box > div > div > div > div > input[type=radio]:nth-child(1)`).click(); // select the reason for submit
-      document.querySelector("#close_confirmed").click(); // submit the test
+      document.querySelector('#activator').click();
+      document.querySelector('input[name="rd"][value="Y"]').click();
+      if(confirm("Are you sure you want to submit?")){
+        document.querySelector('#close_confirmed').click();
+      }
     }
   }
   // 2 seconds(2000 milliseconds) for each question
